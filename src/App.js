f06c83b8616ee0878dd98393c53cbe4568d0a8eb
tejendrasada <tejendrasada1997@gmail.com>
2023-09-30 11:33:13 +0700

    Thank you page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Boxui from './box/boxjsx';
+import './img/flow.png';
+
+
+function App() {
+  return (
+    <div className="mid-box">
+      <Boxui/>
+    </div>
+  );
+}
+
+export default App;
